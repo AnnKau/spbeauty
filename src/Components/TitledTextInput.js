c56ec64d5ae4dfd831f1d39daac1f4e8872dbddd
AnnKau <148041666+AnnKau@users.noreply.github.com>
2023-10-16 10:35:43 +0000

    Обновила TitledSelection

--- src/Components/TitledTextInput.js
+++ src/Components/TitledTextInput.js
@@ -2,9 +2,16 @@ import styled from "styled-components";
 
 const Input = styled.input`
     outline: none;
-    border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
+    padding: 8px 16px;
+    align-items: center;
+    gap: 16px;
+    align-self: stretch;
+    border-radius: 24px;
+    width: 100px;
+    min-width: 50%;
+    max-width: 100%;
+    border: 1px solid var(--steel-gray-200, #E2E8F0);
+    background-color: var(--steel-gray-50, #F8FAFC);
     color: #666787;
     &:hover {
         background-color: #D9E2EFB8;
@@ -12,11 +19,12 @@ const Input = styled.input`
 `;
 
 function TitledTextInput({title, def}) {
+    
     const Title = title === undefined ? 
         (<div></div>) : 
         (<div>{title}</div>);
     return (
-        <div className='d-flex flex-column gap-2'>
+        <div className='d-flex flex-column gap-1'>
             {Title}
             <Input className='p-2' defaultValue={def} ></Input>
         </div>
